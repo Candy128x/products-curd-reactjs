14b4c6fba7b05de9eef4b4dc57aecface9366880
Ashish Sondagar <ashish.sondagar@peopleinteractive.in>
2021-07-10 13:45:49 +0530

    init-code-v2

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-secondary">
           <Link to={"/products"} className="navbar-brand">
-            OroPocket
+            Demo
           </Link>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
